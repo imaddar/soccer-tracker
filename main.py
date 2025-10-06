844fd3a880ce7cdf9d682268f632becf7e8f5b0e
Imad Dar <imaddar@icloud.com>
2025-10-06 02:17:23 -0600

    many changes, finished up to tracking section from video

---
+++ main.py
@@ -0,0 +1,23 @@
+from utils.init import read_video, save_video
+from trackers.init import Tracker
+
+def main():
+    # read video
+    video_frames = read_video("input_videos/08fd33_4.mp4")
+    
+    # initalize tracker
+    tracker = Tracker('models/best.pt')
+    
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/tracks_stub.pkl')
+
+    # draw output
+    ## draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    
+    # save video
+    save_video(output_video_frames, "output_videos/output_video.avi")
+    
+if __name__ == '__main__':
+    main()
