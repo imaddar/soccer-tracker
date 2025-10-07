6fe39f5e50864c556087c670cd5538fa109dbcde
Imad Dar <imaddar@icloud.com>
2025-10-06 23:25:31 -0600

    player team assignment completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.init import read_video, save_video
 from trackers.init import Tracker
+from team_assigner.init import TeamAssigner
 import cv2
 def main():
     # read video
@@ -13,6 +14,18 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/tracks_stub.pkl')
 
+    
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], tracks["players"][0])
+    
+    for frame_num, player_track in enumerate(tracks["players"]):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num], track["bbox"], player_id)
+            tracks["players"][frame_num][player_id]["team"] = team
+            tracks["players"][frame_num][player_id]["team_color"] = team_assigner.team_colors[team]
+            
+            
     
     # draw output
     ## draw object tracks
