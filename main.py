0b53bcdba1bd4939ee516625d11977b36a8385f6
Imad Dar <imaddar@icloud.com>
2025-10-07 00:08:27 -0600

    ball possession overlay finished

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from trackers import Tracker
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 import cv2
+import numpy as np
+
 def main():
     # read video
     video_frames = read_video("input_videos/08fd33_4.mp4")
@@ -32,16 +34,21 @@ def main():
     
     # Assign Ball Aquistion
     player_assigner = PlayerBallAssigner()
+    team_ball_control = []
+    
     for frame_num, player_track in enumerate(tracks["players"]):
         ball_bbox = tracks["ball"][frame_num][1]['bbox']
         assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
         
         if assigned_player != -1:
             tracks["players"][frame_num][assigned_player]["has_ball"] = True
-    
+            team_ball_control.append(tracks["players"][frame_num][assigned_player]["team"])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control = np.array(team_ball_control)
     # draw output
     ## draw object tracks
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control)
     
     # save video
     save_video(output_video_frames, "output_videos/output_video.avi")
